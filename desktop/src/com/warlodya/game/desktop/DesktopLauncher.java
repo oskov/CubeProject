f85065fe61c33af3fb77a7f164348af451d15c27
oskov <faklosg@gmail.com>
2020-03-19 20:21:14 +0200

    Remove unused android support

--- desktop/src/com/warlodya/game/desktop/DesktopLauncher.java
+++ desktop/src/com/warlodya/game/desktop/DesktopLauncher.java
@@ -5,10 +5,8 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.warlodya.game.GameClass;
 
 public class DesktopLauncher {
-    public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 1600;
-        config.height = 900;
-        new LwjglApplication(new GameClass(), config);
-    }
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameClass(), config);
+	}
 }
